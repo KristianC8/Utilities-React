008fbcb0211f830ad5a2dc12355ab03446b6302d
Kristian Cifuentes <kdcv2007@hotmail.com>
2024-08-29 20:16:23 -0500

    change title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <header className='header-main'>
       <div className='header-main-title'>
-        <h1>Utilities React</h1>
+        <h1>Utilidades React</h1>
         <ReactIcon />
       </div>
       <MobileMenu />

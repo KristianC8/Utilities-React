f5b305106439266fe7335a5c7fe1fee2111a4b99
Kristian Cifuentes <kdcv2007@hotmail.com>
2024-08-29 20:45:15 -0500

    config output dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'dist' // Asegúrate de que esto coincida con la carpeta que estás usando
+  }
 })

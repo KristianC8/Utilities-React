694a9638431d331a26324402537cd5c3dab8a135
Kristian Cifuentes <kdcv2007@hotmail.com>
2024-08-31 13:16:08 -0500

    add functionality sort

--- src/components/SortBy.tsx
+++ src/components/SortBy.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { Loader } from './Loader'
 import './SortBy.css'
 
 export const SortBy = () => {
@@ -8,10 +9,13 @@ export const SortBy = () => {
     image: string
   }
   const [characters, setCharacters] = useState<character[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>()
+  const [sort, setSort] = useState<string>('--')
 
   useEffect(() => {
     const getData = async () => {
       try {
+        setIsLoading(true)
         const response = await fetch(
           'https://rickandmortyapi.com/api/character/[1,2,3,4,5,11,7,72,9,10,78,91]'
         )
@@ -23,25 +27,61 @@ export const SortBy = () => {
         setCharacters(data)
       } catch (error) {
         console.log(error)
+      } finally {
+        setIsLoading(false)
       }
     }
-
     getData()
   }, [])
+
+  const changedSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSort(event.target.value)
+  }
+
+  const sortedCharacters = (sort: string) => {
+    if (sort === '--') return characters
+    if (sort === 'a-z') {
+      return [...characters].sort((a, b) => a.name.localeCompare(b.name))
+    }
+    if (sort === 'z-a') {
+      return [...characters].sort((b, a) => a.name.localeCompare(b.name))
+    }
+    if (sort === 'id+') {
+      return [...characters].sort((a, b) => a.id - b.id)
+    }
+    if (sort === 'id-') {
+      return [...characters].sort((b, a) => a.id - b.id)
+    }
+  }
 
   return (
     <>
       <h3>Ordenar:</h3>
       <div className='sort-container'>
-        <section className='characters-grid'>
-          {characters &&
-            characters.map((character) => (
+        <div className='sort-select'>
+          <label htmlFor='ordenar'>Ordenar por:</label>
+          <select id='ordenar' name='ordenar' onChange={changedSelect}>
+            <option defaultValue='--'>--</option>
+            <option value='a-z'>A - Z</option>
+            <option value='z-a'>Z - A</option>
+            <option value='id+'>Id ⬆️</option>
+            <option value='id-'>Id ⬇️</option>
+          </select>
+        </div>
+
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <section className='characters-grid'>
+            {sortedCharacters(sort)?.map((character) => (
               <article key={character.id}>
                 <span>{character.name}</span>
                 <img src={character.image} alt={character.name} />
+                Id:{` ${character.id}`}
               </article>
             ))}
-        </section>
+          </section>
+        )}
       </div>
     </>
   )

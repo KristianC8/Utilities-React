7c1fc9422a0a89259bd7cc257a942b59e5952683
Kristian Cifuentes <kdcv2007@hotmail.com>
2024-09-11 17:31:04 -0500

    add progress bar header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,47 @@
 import { ReactIcon } from './icons/ReactIcon'
 import { MobileMenu } from './MobileMenu'
 import './Header.css'
-import { useMobileMenuCtx } from '../hooks/useMobileMenuCtx'
+import 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js'
+import { useEffect, useRef } from 'react'
+// import { useMobileMenuCtx } from '../hooks/useMobileMenuCtx'
+
+declare global {
+  interface Window {
+    ScrollTimeline: any // eslint-disable-line @typescript-eslint/no-explicit-any
+  }
+}
 
 export const Header = () => {
-  const { isOpen } = useMobileMenuCtx()
+  // const { isOpen } = useMobileMenuCtx()
+  const progressBar = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    if (progressBar.current) {
+      progressBar.current.animate(
+        { width: ['0%', '100%'] },
+        {
+          timeline: new window.ScrollTimeline({
+            source: document.documentElement
+          })
+        }
+      )
+    }
+  }, [])
   return (
-    <header
-      className='header-main'
-      style={{
-        borderRadius: `${isOpen ? '0' : '0 0 0.5rem 0.5rem'}`
-      }}
-    >
-      <div className='header-main-title'>
-        <h1>Componentes React</h1>
-        <ReactIcon />
-      </div>
-      <MobileMenu />
-    </header>
+    <>
+      <header
+        className='header-main'
+        // style={{
+        //   borderRadius: `${isOpen ? '0' : '0 0 0.5rem 0.5rem'}`
+        // }}
+      >
+        <div className='header-main-title'>
+          <h1>Componentes React</h1>
+          <ReactIcon />
+        </div>
+        <MobileMenu />
+        <div className='header-progress' ref={progressBar}></div>
+      </header>
+    </>
   )
 }
